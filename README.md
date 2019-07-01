c82c5e8418e9edab76671ed5fdd2511d03c8df9f
王莹莹 <wangyingying11@58ganji.com>
2019-07-01 18:16:05 +0800

    fix: 添加示例

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 一个具有图片旋转、压缩、下载功能的h5上传组件
 
 # 功能说明
-
 - 项目基于vue
 - 可用于pc端、h5
 - 当上传图片的时候会根据limitsize属性对图片进行压缩
@@ -10,7 +9,6 @@
 - 可对已上传的图片进行下载
 
 # 样式截图
-
-
+![image](https://github.com/yywang95/h5ImageUploader/blob/master/assets/cut.png)
 
 `注意`：仅上传部分代码，不可直接运行，请根据自己项目的接口进行参考～
---
+++ pages/test.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <Images
+            :id="'pics'"
+            :items="pics"
+            @on-error="listenUploadError"
+            @on-success="listenUploadSuccess"
+            @on-delete="listenUploadDelete" />
+    </div>
+</template>
+<script>
+import Images from '@/components/Images';
+
+export default {
+    data() {
+        return {
+            pics: [],
+        };
+    },
+    methods: {
+        /** @desc 图片上传错误 */
+        listenUploadError(id, msg, item) {
+            console.error(`图片${item.name}上传失败：${msg}`);
+        },
+        /** @desc 图片上传成功 */
+        listenUploadSuccess(id, urls) {
+            this[id].push(...urls);
+        },
+        /** @desc 图片删除 */
+        listenUploadDelete(id, item, index) {
+            this[id].splice(index, 1);
+        },
+    },
+};
+</script>
